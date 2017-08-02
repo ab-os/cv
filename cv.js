6949a17c59404c6427312ae01bc4d155892dc2d7
ab-os <arjen@bos4.nl>
2017-08-02 16:48:55 +0200

    Only flexbox doorgevoerd. W3.CSS nog wel behouden want doet goede dingen voor het font blijkbaar

--- cv.js
+++ cv.js
@@ -1,9 +1,16 @@
 // Replace all hyphens in h3 with ndash
-$("h3").each( function() {
+$(".cv-section > h3 > .date").each( function() {
 	var text = $(this).text();
     text = text.replace("-", "&ndash;");
     $(this).html(text);
 });
 
-// Add icon to h3
-$("h3").prepend('<i class="fa fa-angle-right"></i>&nbsp;');
+// Format h3
+$(".cv-section > h3").prepend('<i class="fa fa-angle-right"></i>&nbsp;');
+$(".cv-section > h3 > .date").addClass("flex-no-shrink right-align italic");
+
+// Add icon to h2, expect for the no-icon
+$(".cv-section > h2").not(".no-icon").prepend('<i class="fa fa-sticky-note margin-right"></i>&nbsp;');
+
+
+
