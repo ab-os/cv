03b51d3a6233175afa31dbf7ae286a83d046b098
ab-os <arjen@bos4.nl>
2017-08-08 17:28:04 +0200

    Added some PDFs and changes some <strong>s

--- cv.js
+++ cv.js
@@ -12,5 +12,19 @@ $(".cv-section > h3 > .date").addClass("flex-no-shrink right-align italic");
 // Add icon to h2, expect for the no-icon
 $(".cv-section > h2").not(".no-icon").prepend('<i class="fa fa-sticky-note margin-right"></i>&nbsp;');
 
+// Get the HTML language
+var lang = document.documentElement.lang;
+// Hide all other languages
+$("*").not(":lang(" + lang + ")").hide();
 
-
+// If English replace the date months
+if(lang == "en") {
+	$(".cv-section > h3 > .date").each( function() {
+		var text = $(this).text();
+		text = text.replace("mei", "may");
+		text = text.replace("juni", "june");
+		text = text.replace("juli", "july");
+		text = text.replace("okt", "oct");
+		$(this).html(text);
+	});
+}
